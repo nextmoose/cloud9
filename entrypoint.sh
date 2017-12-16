55f20eaef5220b661833ffaa4ee37a7ba9f2255c
Emory Merryman <emory.merryman@gmail.com>
2017-12-16 03:44:06 +0000

    if no --extension then we should execute anyway

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,7 +25,8 @@ do
                         sh /opt/docker/extension/init.user.sh "${@}"
                     fi &&
                     PROJECT_NAME="${PROJECT_NAME}" node /opt/docker/c9sdk/server.js --listen 0.0.0.0 -w /opt/docker/workspace -p ${CLOUD9_PORT} &&
-                    shift ${#}
+                    shift ${#} &&
+                    exit 0
         ;;
         *)
             echo Unknown Option &&
@@ -34,4 +35,17 @@ do
                 exit 64
         ;;
     esac
-done
+done &&
+    if [ -f /opt/docker/extension/cleanup.sh ]
+    then
+        cleanup() {
+            sh /opt/docker/extension/cleanup.sh
+        } &&
+        trap cleanup EXIT
+    fi &&
+        if [ -f /opt/docker/extension/init.user.sh ]
+        then
+            sh /opt/docker/extension/init.user.sh "${@}"
+        fi &&
+        PROJECT_NAME="${PROJECT_NAME}" node /opt/docker/c9sdk/server.js --listen 0.0.0.0 -w /opt/docker/workspace -p ${CLOUD9_PORT} &&
+        shift ${#}
