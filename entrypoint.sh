17c43d797403a3938ceef1dba55ffd8baf313fb7
Emory Merryman <emory.merryman@gmail.com>
2017-12-21 02:19:48 +0000

    remove arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,53 +1,15 @@
 #!/bin/sh
 
-while [ ${#} -gt 0 ]
-do
-    case ${1} in
-        --project-name)
-            PROJECT_NAME="${2}" &&
-                shift 2
-        ;;
-        --cloud9-port)
-            CLOUD9_PORT="${2}" &&
-                shift 2
-        ;;
-        --extension)
-            shift &&
-                if [ -f /opt/docker/extension/cleanup.sh ]
-                then
-                    cleanup() {
-                        sh /opt/docker/extension/cleanup.sh
-                    } &&
-                    trap cleanup EXIT
-                fi &&
-                    if [ -f /opt/docker/extension/init.user.sh ]
-                    then
-                        sh /opt/docker/extension/init.user.sh "${@}"
-                    fi &&
-                    echo A PROJECT_NAME=${PROJECT_NAME} CLOUD9_PORT=${CLOUD9_PORT} &&
-                    PROJECT_NAME="${PROJECT_NAME}" node /opt/docker/c9sdk/server.js --listen 0.0.0.0 -w /opt/docker/workspace -p ${CLOUD9_PORT} &&
-                    shift ${#} &&
-                    exit 0
-        ;;
-        *)
-            echo Unknown Option &&
-                echo ${0} &&
-                echo ${@} &&
-                exit 64
-        ;;
-    esac
-done &&
-    if [ -f /opt/docker/extension/cleanup.sh ]
+if [ -f /opt/docker/extension/cleanup.sh ]
+then
+    cleanup() {
+        sh /opt/docker/extension/cleanup.sh
+    } &&
+    trap cleanup EXIT
+fi &&
+    if [ -f /opt/docker/extension/init.user.sh ]
     then
-        cleanup() {
-            sh /opt/docker/extension/cleanup.sh
-        } &&
-        trap cleanup EXIT
+        sh /opt/docker/extension/init.user.sh "${@}"
     fi &&
-        if [ -f /opt/docker/extension/init.user.sh ]
-        then
-            sh /opt/docker/extension/init.user.sh "${@}"
-        fi &&
-        echo B PROJECT_NAME=${PROJECT_NAME} CLOUD9_PORT=${CLOUD9_PORT} &&
-        PROJECT_NAME="${PROJECT_NAME}" node /opt/docker/c9sdk/server.js --listen 0.0.0.0 -w /opt/docker/workspace -p ${CLOUD9_PORT} &&
-        shift ${#}
+    PROJECT_NAME="${PROJECT_NAME}" node /opt/docker/c9sdk/server.js --listen 0.0.0.0 -w /opt/docker/workspace -p ${CLOUD9_PORT} &&
+    shift ${#}
