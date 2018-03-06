baf1bd20e2d56f7f3ff1b04b7e5bb31efa632b66
Emory Merryman <emory.merryman@gmail.com>
2018-03-06 13:22:39 +0000

    preparation commit

--- entrypoint.root.sh
+++ entrypoint.root.sh
@@ -2,16 +2,12 @@
 
 if [ -f /home/user/extension/init.root.sh ]
 then
-    sh /home/user/extension/init.root.sh
+    sh /home/user/extension/init.root.sh "${@}"
 fi &&
     if [ -f /home/user/extension/user.sudo ]
     then
         cat /home/user/extension/user.sudo > /etc/sudoers.d/user &&
             chmod 0444 /etc/sudoers.d/user
-    fi &&
-    if [ -f /home/user/extension/user.id ]
-    then
-        usermod -u $(cat /home/user/extension/user.id) user
     fi &&
     su -c "sh /home/user/scripts/entrypoint.user.sh \${@}" &&
     shift ${#}
